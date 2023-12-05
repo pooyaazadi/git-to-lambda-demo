e9a8660b71f579bc3be3b7a8fe76404bb4f7671e
Pooya Azadi <pooya.azadi@Helpdesks-MacBook-Pro-3.local>
2023-12-05 16:56:29 -0500

    some changes for testing

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    print('The lambda function has changed')
+    print('The lambda function has changed!')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
